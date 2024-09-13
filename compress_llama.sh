c7dc07849dc86ce6d9b1de8f9c157c95e2d22662
Xin Wang <40883104+tuidan@users.noreply.github.com>
2024-09-13 01:29:34 -0400

    Update compress_llama.sh

--- compress_llama.sh
+++ compress_llama.sh
@@ -6,6 +6,6 @@ python SVDLLM.py --model jeffwan/llama-7b-hf --step 1 --ratio 0.2 --whitening_ns
 # python SVDLLM.py --model jeffwan/llama-7b-hf --step 1 --ratio 0.2 --whitening_nsamples 256 --dataset wikitext2 --model_seq_len 2048 --save_path ./ --run_low_resource
 
 # evaluate the perplexity of llama_7b_whitening_0.2.pt
-python SVDLLM.py --step 4 --model_path whitening/jeffwan_llama_7b_hf_whitening_0.2.pt
+python SVDLLM.py --step 4 --model_path whitening/jeffwan_llama_7b_hf_whitening_0.8.pt
 # evaluate the efficiency of llama_7b_whitening_0.2.pt
-python SVDLLM.py --step 5 --model_path whitening/jeffwan_llama_7b_hf_whitening_0.2.pt
+python SVDLLM.py --step 5 --model_path whitening/jeffwan_llama_7b_hf_whitening_0.8.pt
